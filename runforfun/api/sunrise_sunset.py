65899fbf8adf0883b6d0454a8946c45c83874a00
Christina Sickelco <csickelco@gmail.com>
2021-04-19 20:08:03 -0400

    Implemented sunrise_sunset api wrapper

---
+++ runforfun/api/sunrise_sunset.py
@@ -0,0 +1,54 @@
+import requests
+from datetime import datetime
+from dateutil import tz
+
+# Constants
+ISO_8601_FORMAT = "%Y-%m-%dT%H:%M:%S%z"
+URL = "https://api.sunrise-sunset.org/json"
+
+
+class SunriseSunset:
+
+    def __init__(self, sunrise, sunset, civilTwilightBegin, civilTwilightEnd):
+        self.sunrise = sunrise
+        self.sunset = sunset
+        self.civilTwilightBegin = civilTwilightBegin
+        self.civilTwilightEnd = civilTwilightEnd
+
+    def info(self):
+        return {
+            'sunrise': self.sunrise,
+            'sunset': self.sunset,
+            'civilTwilightBegin': self.civilTwilightBegin,
+            'civilTwilightEnd': self.civilTwilightEnd
+        }
+
+
+latitude = "43.161030"
+longitude = "-77.610924"
+dateVal = "tomorrow"
+
+
+def get_sunrise_sunset():
+
+    to_zone = tz.gettz('America/New_York')
+
+    call_params = {'lat': latitude, 'lng': longitude, 'date': dateVal, 'formatted': 0}
+
+    # sending get request and saving the response as response object
+    r = requests.get(url=URL, params=call_params)
+
+    # extracting data in json format
+    data = r.json()
+
+    # extracting sunrise, civil_twilight_begin
+    sunrise_utc = datetime.strptime(data['results']['sunrise'], ISO_8601_FORMAT)
+    sunrice_local = sunrise_utc.astimezone(to_zone)
+    sunset_utc = datetime.strptime(data['results']['sunset'], ISO_8601_FORMAT)
+    sunset_local = sunset_utc.astimezone(to_zone)
+    civil_twilight_begin_utc = datetime.strptime(data['results']['civil_twilight_begin'], ISO_8601_FORMAT)
+    civil_twilight_begin_local = civil_twilight_begin_utc.astimezone(to_zone)
+    civil_twilight_end_utc = datetime.strptime(data['results']['civil_twilight_end'], ISO_8601_FORMAT)
+    civil_twilight_end_local = civil_twilight_end_utc.astimezone(to_zone)
+
+    return SunriseSunset(sunrice_local, sunset_local, civil_twilight_begin_local, civil_twilight_end_local)
--- runforfun/runforfun.py
+++ runforfun/runforfun.py
@@ -1,5 +1,6 @@
 import sys
 from util import emailer
+from api import sunrise_sunset
 
 if len(sys.argv) < 4:
     print("Usage: python3 runforfun/runforfun.py {send_email_address} {receiver_email} {password")
@@ -9,17 +10,27 @@ sender_email = sys.argv[1]
 receiver_email = sys.argv[2]
 password = sys.argv[3]
 
+print("Running for runforfun...")
+sunriseSunset = sunrise_sunset.get_sunrise_sunset()
+print(f'Sunrise/Sunst: {sunriseSunset.info()}')
+
 # Create the plain-text and HTML version of your message
 text = """\
 This is your run reminder from runforfun!"""
-html = """\
+html = f"""\
 <html>
   <body>
     <p>This is your run reminder from runforfun!</p>
+    <b>Sunrise/Sunset</b>
+    <ul>
+        <li>Sunrise: {sunriseSunset.sunrise}</li>
+        <li>Sunset: {sunriseSunset.sunset}</li>
+        <li>Twilight Begin: {sunriseSunset.civilTwilightBegin}</li>
+        <li>Twilight End: {sunriseSunset.civilTwilightEnd}</li>
+    </ul>
   </body>
 </html>
 """
-
-print("Running for runforfun...")
 emailer.send_email(sender_email, receiver_email, password, "Run Reminder", text, html)
+
 print("Successfully completed runforfun")
