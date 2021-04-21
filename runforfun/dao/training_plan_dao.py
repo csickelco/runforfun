50048f6ec67fe5f28bc7ad4158b97d3aa9d73d25
Christina Sickelco <csickelco@gmail.com>
2021-04-20 20:05:35 -0400

    Implemented basic training plan reader

---
+++ runforfun/dao/training_plan_dao.py
@@ -0,0 +1,38 @@
+import csv
+from datetime import datetime
+
+class Workout:
+
+    def __init__(self, workoutDateTime, distance, duration, notes):
+        self.workoutDateTime = workoutDateTime
+        self.distance = distance
+        self.duration = duration
+        self.notes = notes
+
+    def info(self):
+        return {
+            'workoutDateTime': self.workoutDateTime,
+            'distance': self.distance,
+            'duration': self.duration,
+            'notes': self.notes
+        }
+
+
+def get_workout_for_date(training_plan_path, desired_workout_date):
+    retval = None
+    with open(training_plan_path) as csvfile:
+        trainingPlanReader = csv.reader(csvfile)
+        # Skip first row (header)
+        next(trainingPlanReader)
+        for row in trainingPlanReader:
+            workout_date_time = datetime.strptime(f'{row[0]} {row[1]}', '%m/%d/%y %I:%M %p')
+            if( desired_workout_date == workout_date_time.date() ):
+                distance = None
+                duration = None
+                if(len(row[2]) > 0 ):
+                    distance = int(row[2])
+                if (len(row[3]) > 0):
+                    duration = int(row[3])
+                notes = row[4]
+                retval = Workout(workout_date_time, distance, duration, notes)
+    return retval
--- runforfun/runforfun.py
+++ runforfun/runforfun.py
@@ -1,36 +1,50 @@
 import sys
 from util import emailer
 from api import sunrise_sunset
+from dao import training_plan_dao
+import datetime
 
-if len(sys.argv) < 4:
-    print("Usage: python3 runforfun/runforfun.py {send_email_address} {receiver_email} {password")
+if len(sys.argv) < 5:
+    print("Usage: python3 runforfun/runforfun.py {send_email_address} {receiver_email} {password} {trainingPlan}")
     sys.exit(0)
 
 sender_email = sys.argv[1]
 receiver_email = sys.argv[2]
 password = sys.argv[3]
+training_plan = sys.argv[4]
 
 print("Running for runforfun...")
-sunriseSunset = sunrise_sunset.get_sunrise_sunset()
-print(f'Sunrise/Sunst: {sunriseSunset.info()}')
 
-# Create the plain-text and HTML version of your message
-text = """\
-This is your run reminder from runforfun!"""
-html = f"""\
-<html>
-  <body>
-    <p>This is your run reminder from runforfun!</p>
-    <b>Sunrise/Sunset</b>
-    <ul>
-        <li>Sunrise: {sunriseSunset.sunrise}</li>
-        <li>Sunset: {sunriseSunset.sunset}</li>
-        <li>Twilight Begin: {sunriseSunset.civilTwilightBegin}</li>
-        <li>Twilight End: {sunriseSunset.civilTwilightEnd}</li>
-    </ul>
-  </body>
-</html>
-"""
-emailer.send_email(sender_email, receiver_email, password, "Run Reminder", text, html)
+sunriseSunset = sunrise_sunset.get_sunrise_sunset()
+tomorrow = datetime.date.today() + datetime.timedelta(days=1)
+workout = training_plan_dao.get_workout_for_date(training_plan, tomorrow)
+
+if workout is None:
+    print('No planned workout for tomorrow')
+else:
+    # Create the plain-text and HTML version of your message
+    text = """\
+    This is your run reminder from runforfun!"""
+    html = f"""\
+    <html>
+      <body>
+        <p>This is your run reminder from runforfun!</p>
+        <b>Sunrise/Sunset:</b>
+        <ul>
+            <li>Sunrise: {sunriseSunset.sunrise}</li>
+            <li>Sunset: {sunriseSunset.sunset}</li>
+            <li>Twilight Begin: {sunriseSunset.civilTwilightBegin}</li>
+            <li>Twilight End: {sunriseSunset.civilTwilightEnd}</li>
+        </ul>
+        <p><b>Workout:</b></p>
+        <ul>
+            <li>Distance: {workout.distance} miles</li>
+            <li>Duration: {workout.duration} minutes</li>
+            <li>Notes: {workout.notes}</li>
+        </ul>
+      </body>
+    </html>
+    """
+    emailer.send_email(sender_email, receiver_email, password, "Run Reminder", text, html)
 
 print("Successfully completed runforfun")
