038b0b687652de470688aef1ca812ee2305c68ea
Christina Sickelco <csickelco@gmail.com>
2021-04-24 12:58:42 -0400

    Implemented weather

--- runforfun/runforfun.py
+++ runforfun/runforfun.py
@@ -27,8 +27,7 @@ workout = training_plan_dao.get_workout_for_date(training_plan, tomorrow)
 if workout is None:
     print('No planned workout for tomorrow')
 else:
-    hourly_weather = weather.get_weather(weather_api_key, latitude, longitude, workout.workout_date_time)
-    print(hourly_weather.info())
+    forecast = weather.get_weather(weather_api_key, latitude, longitude, workout.workout_date_time)
 
     # Create the plain-text and HTML version of your message
     text = """\
@@ -37,6 +36,23 @@ else:
     <html>
       <body>
         <p>This is your run reminder from runforfun!</p>
+        <p><b>Workout:</b></p>
+        <ul>
+            <li>Distance: {workout.distance} miles</li>
+            <li>Duration: {workout.duration} minutes</li>
+            <li>Time: {workout.workout_date_time}</li>
+            <li>Notes: {workout.notes}</li>
+        </ul>
+        <p><b>Weather:</b></p>
+        <ul>
+            <li>Temperature: {forecast.temp}</li>
+            <li>Feels Like Temperature: {forecast.feels_like}</li>
+            <li>Wind Speed: {forecast.wind_speed}</li>
+            <li>Wind Gust: {forecast.wind_gust}</li>
+            <li>Precipitation Probability: {forecast.precipitation_probability}</li>
+            <li>Description: {forecast.weather[0].description}</li>
+            <li>Icon: {forecast.weather[0].icon}</li>
+        </ul>
         <b>Sunrise/Sunset:</b>
         <ul>
             <li>Sunrise: {sunriseSunset.sunrise}</li>
@@ -44,15 +60,9 @@ else:
             <li>Twilight Begin: {sunriseSunset.civilTwilightBegin}</li>
             <li>Twilight End: {sunriseSunset.civilTwilightEnd}</li>
         </ul>
-        <p><b>Workout:</b></p>
-        <ul>
-            <li>Distance: {workout.distance} miles</li>
-            <li>Duration: {workout.duration} minutes</li>
-            <li>Notes: {workout.notes}</li>
-        </ul>
       </body>
     </html>
     """
-    #emailer.send_email(sender_email, receiver_email, password, "Run Reminder", text, html)
+    emailer.send_email(sender_email, receiver_email, password, "Run Reminder", text, html)
 
 print("Successfully completed runforfun")
